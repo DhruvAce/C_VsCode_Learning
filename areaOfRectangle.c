8dad236578a2018d43d7869f42b60a981d8a4745
Dhruv Panchal <dgapdhruv@gmail.com>
2026-07-17 19:32:37 +0530

    Change areaOfRectangle.c

--- areaOfRectangle.c
+++ areaOfRectangle.c
@@ -18,6 +18,9 @@ int main()
 
     printf("The area of rectangle for length \"%.5f\" and width \"%.5f\" is : \"%.5f\"\n", length, width, calculateArea(length, width));
 
+    printf("\nPress Enter to exit...");
+    getchar(); // Consumes the Enter left by scanf
+    getchar(); // Waits for you to press Enter
     
     return 0;
 }
